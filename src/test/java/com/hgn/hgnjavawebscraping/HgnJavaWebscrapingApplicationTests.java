1959d35bf5720da81bff356aaf14c6d99ddef5cd
humbertoguerrer <humberto.guerrer@gmail.com>
2021-06-10 21:18:17 -0300

    Projeto criado

---
+++ src/test/java/com/hgn/hgnjavawebscraping/HgnJavaWebscrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.hgnjavawebscraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HgnJavaWebscrapingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
